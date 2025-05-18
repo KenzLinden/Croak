8950177fe0555afc584aa26ad74368696f01b3df
Mackenzie Linden <mlinden1403@gmail.com>
2025-05-18 21:59:55 +1000

    poetry upgrades, django app, docker fixes

---
+++ backend/croak_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CroakAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "croak_app"
--- backend/croak_site/asgi.py
+++ backend/croak_site/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for croak project.
+ASGI config for croak_site project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'croak.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "croak_site.settings")
 
 application = get_asgi_application()
